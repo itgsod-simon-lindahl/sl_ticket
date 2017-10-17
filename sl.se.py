4dc1df6476b241079d9ba99b67d64cdba2487cac
simon.lindahl <simon.lindahl@elev.it-gymnasiet.se>
2017-10-17 10:46:02 +0200

    fixed sulution

--- sl.se.py
+++ sl.se.py
@@ -1,11 +1,17 @@
-def fares(age, student=False, senior=False):
-    print age,student,senior
-    return 'hel'
+def fares(age, disscount=False):
+    if age >=20 and age < 65:
+        disscount = False
+    elif age >= 65 or age < 20:
+        disscount = True
 
-
-
-
+    if disscount:
+        print "halv"
+        return "halv"
+    else:
+        print "hel"
+        return "hel"
 
 fares(10)
-fares(65,senior=True)
-fares(25,student=True)
+fares(65)
+fares(25)
+
