2ed8d8477d449298e356f50a0a0748427a92e863
simon.lindahl <simon.lindahl@elev.it-gymnasiet.se>
2017-10-25 13:00:43 +0200

    ändrade så äldre studenter och yngre pensionär fortfarande kan få rabatt.

--- sl.se.py
+++ sl.se.py
@@ -1,10 +1,9 @@
-def fares(age, disscount=False):
+def fares(age, disscount=False, student=False, senior=False):
     if age >=20 and age < 65:
         disscount = False
     elif age >= 65 or age < 20:
         disscount = True
-
-    if disscount:
+    if disscount or student or senior:
         print "halv"
         return "halv"
     else:
@@ -13,5 +12,4 @@ def fares(age, disscount=False):
 
 fares(10)
 fares(65)
-fares(25)
-
+fares(25, student=True)
